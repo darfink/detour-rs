6182fc5f2f621eea3d11eb4e5420fee05cbfdbca
Marc Addeo <marcaddeo@gmail.com>
2018-12-02 13:49:18 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![recursion_limit = "1024"]
 #![cfg_attr(feature = "nightly", feature(const_fn, unboxed_closures, abi_thiscall))]
-#![cfg_attr(all(feature = "nightly", test), feature(naked_functions, core_intrinsics, asm, abi_thiscall))]
+#![cfg_attr(all(feature = "nightly", test), feature(naked_functions, core_intrinsics, asm))]
 
 //! A cross-platform detour library written in Rust.
 //!

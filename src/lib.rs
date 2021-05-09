cf4cc95113fbce4423a865524862925666c95cec
super-continent <46540468+super-continent@users.noreply.github.com>
2021-05-08 19:28:22 -0700

    Add unstable feature use of `const_fn_trait_bound`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![recursion_limit = "1024"]
-#![cfg_attr(feature = "nightly", feature(const_fn, unboxed_closures, abi_thiscall))]
+#![cfg_attr(feature = "nightly", feature(const_fn, unboxed_closures, abi_thiscall, const_fn_trait_bound))]
 #![cfg_attr(
   all(feature = "nightly", test),
   feature(naked_functions, core_intrinsics, llvm_asm)

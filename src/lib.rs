9304c628056b72065bd1add8500959102128eeaf
Mason Ginter <mason@dagint.com>
2022-11-14 18:09:44 -0500

    remove const_fn_trait_bound since no longer needed on nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![recursion_limit = "1024"]
-#![cfg_attr(
-  feature = "nightly",
-  feature(const_fn_trait_bound, unboxed_closures, abi_thiscall)
-)]
+#![cfg_attr(feature = "nightly", feature(unboxed_closures, abi_thiscall))]
 #![cfg_attr(
   all(feature = "nightly", test),
   feature(naked_functions, core_intrinsics, asm)
